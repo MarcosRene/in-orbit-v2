0ea85cdd7aaba9111787b9fb034b3fb50e8d2c5d
Marcos Rene <renemoura789@gmail.com>
2025-01-18 12:31:13 -0300

    test: authenticate from github code

---
+++ api/src/app/functions/authenticante-from-github-code.spec.ts
@@ -0,0 +1,76 @@
+import { describe, it, expect, vi } from 'vitest'
+import { and, eq, ne } from 'drizzle-orm'
+import { beforeEach } from 'node:test'
+
+import { db } from '../../db'
+import { users } from '../../db/schema'
+import { authenticateFromGithubCode } from './authenticante-from-github-code'
+
+import * as githubOauth from '../../modules/github-oauth'
+import { makeUser } from '../../../tests/factories/make-user'
+
+describe('Authenticate From Github Code', () => {
+  beforeEach(async () => {
+    vi.mock('../modules/github-oauth')
+
+    vi.clearAllMocks()
+  })
+
+  it('should be able to authenticate from github code', async () => {
+    vi.spyOn(githubOauth, 'getUserFromAccessToken').mockResolvedValueOnce({
+      id: 123456789,
+      name: 'John Doe',
+      email: null,
+      avatar_url: 'https://github.com/MarcosRene.png',
+    })
+
+    const sut = await authenticateFromGithubCode({
+      code: 'sample-github-code',
+    })
+
+    expect(sut.token).toEqual(expect.any(String))
+
+    const [userOnDb] = await db
+      .select()
+      .from(users)
+      .where(eq(users.externalAccountId, 123456789))
+
+    expect(userOnDb.name).toEqual('John Doe')
+  })
+
+  it('should be able to authenticate when if existing github user', async () => {
+    const existing = await makeUser({
+      name: 'Jane Doe',
+    })
+
+    await db
+      .delete(users)
+      .where(
+        and(
+          eq(users.externalAccountId, existing.externalAccountId),
+          ne(users.id, existing.id)
+        )
+      )
+
+    vi.spyOn(githubOauth, 'getUserFromAccessToken').mockResolvedValueOnce({
+      id: existing.externalAccountId,
+      name: 'Jane Doe',
+      email: null,
+      avatar_url: 'https://github.com/MarcosRene.png',
+    })
+
+    const sut = await authenticateFromGithubCode({
+      code: 'sample-github-code',
+    })
+
+    expect(sut.token).toEqual(expect.any(String))
+
+    const [userOnDb] = await db
+      .select()
+      .from(users)
+      .where(eq(users.externalAccountId, existing.externalAccountId))
+
+    expect(userOnDb.name).toEqual('Jane Doe')
+  })
+})
+
