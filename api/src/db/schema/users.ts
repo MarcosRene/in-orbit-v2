b2b92de6c3f59bf9bedfd3eaead72c88bb221fce
Marcos Rene <renemoura789@gmail.com>
2025-01-18 13:09:36 -0300

    feat: add expirience column in users table

--- api/src/db/schema/users.ts
+++ api/src/db/schema/users.ts
@@ -1,4 +1,5 @@
 import { createId } from '@paralleldrive/cuid2'
+import { integer } from 'drizzle-orm/pg-core'
 import { pgTable, text } from 'drizzle-orm/pg-core'
 
 export const users = pgTable('users', {
@@ -8,5 +9,7 @@ export const users = pgTable('users', {
   name: text('name'),
   email: text('email'),
   avatarUrl: text('avatar_url').notNull(),
+  experience: integer('experience').notNull().default(0),
   externalAccountId: text('external_account_id').notNull().unique(),
 })
+
